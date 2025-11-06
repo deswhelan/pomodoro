b740a20511d4751fd7e3f9485b4a2830e52b44ec
Des Whelan <wheland6@tcd.ie>
2025-11-06 11:23:47 +0000

    create window and canvas

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
+FONT_NAME = "Arial"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
@@ -16,3 +16,15 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.minsize(height=475, width=300)
+window.config(background=YELLOW, pady=50)
+
+canvas = Canvas(width=205, height=224, background=YELLOW, highlightthickness=0)
+TOMATO_IMAGE = PhotoImage(file="tomato.png")
+canvas.create_image(102, 112, image=TOMATO_IMAGE)
+canvas.create_text(102, 130, text="00:00", font=(FONT_NAME, 30, "bold"), fill=YELLOW)
+canvas.pack()
+
+window.mainloop()
