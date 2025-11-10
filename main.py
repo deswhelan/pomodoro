3a9ffb7c08b528e3cec477c78d7b25b31d7038f9
Des Whelan <wheland6@tcd.ie>
2025-11-10 18:10:40 +0000

    reset timer when reset button clicked

--- main.py
+++ main.py
@@ -14,22 +14,36 @@ WORK_REPS = [1, 3, 5, 7]
 SHORT_BREAK_REPS = [2, 4, 6]
 current_rep = 1
 work_reps_completed = ""
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    global timer
+    global work_reps_completed
+
+    window.after_cancel(timer)
+
+    timer_label.config(text="Timer", fg=GREEN)
+    canvas.itemconfig(timer_text, text=f"00:00")
+    work_reps_completed = ""
+    display_completed_work_reps()
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
     global current_rep
+    global work_reps_completed
 
     if current_rep in WORK_REPS:
         timer_label.config(text="WORK", fg=GREEN)
         count_down(WORK_MINUTES * 60)
     elif current_rep in SHORT_BREAK_REPS:
+        work_reps_completed += "✓"
         display_completed_work_reps()
         timer_label.config(text="SHORT\nBREAK", fg=PINK)
         count_down(SHORT_BREAK_MINUTES * 60)
     else:
         # We can assume we are now at rep 8, aka "long" break
+        work_reps_completed += "✓"
         display_completed_work_reps()
         current_rep = 0
         timer_label.config(text="LONG\nBREAK", fg=RED)
@@ -37,12 +51,12 @@ def start_timer():
 
 def display_completed_work_reps():
     global work_reps_completed
-    work_reps_completed += "✓"
     pomodoros_completed_label.config(text=work_reps_completed)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     global current_rep
+    global timer
 
     if count >= 0:
         minutes = math.floor(count/60)
@@ -56,9 +70,9 @@ def count_down(count):
 
         if seconds == "00" and minutes == 0:
             current_rep += 1
-            window.after(COUNTDOWN_STEP_MILLISECONDS, start_timer)
+            timer = window.after(COUNTDOWN_STEP_MILLISECONDS, start_timer)
         else:
-            window.after(COUNTDOWN_STEP_MILLISECONDS, count_down, count - 1)
+            timer = window.after(COUNTDOWN_STEP_MILLISECONDS, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -78,8 +92,6 @@ timer_label.grid(column=2, row=1)
 start_button = Button(text="Start", command=start_timer, background=PINK, foreground=YELLOW)
 start_button.grid(column=1, row=3)
 
-def reset_timer():
-    print("Timer reset!")
 start_button = Button(text="Reset", command=reset_timer, background=PINK, foreground=YELLOW)
 start_button.grid(column=3, row=3)
 
